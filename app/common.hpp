e3d0f54a07dd76fc689b76fb249b4eeab7102a78
flyingsl0ths <randomemail33110@protonmail.com>
2024-04-16 06:19:43 -0700

    Remove unused entries

--- app/common.hpp
+++ app/common.hpp
@@ -5,9 +5,8 @@
 
 using u8 [[maybe_unused]]    = char8_t;
 using u32 [[maybe_unused]]   = unsigned int;
-using usz [[maybe_unused]]   = std::size_t;
 using s32 [[maybe_unused]]   = int;
 using f32 [[maybe_unused]]   = float;
 using f64 [[maybe_unused]]   = double;
-using usize [[maybe_unused]] = std::size_t;
+using usize [[maybe_unused]] = size_t;
 using str [[maybe_unused]]   = std::string_view;
